59bc0581888ca3ee191332cdd03f6703bd7291b4
az <abdl.alaziz95@gmail.com>
2025-05-30 04:50:21 +0700

    add cors

--- src/main/java/com/example/library/controller/CorsFallBackController.java
+++ src/main/java/com/example/library/controller/CorsFallBackController.java
@@ -1,22 +1,22 @@
-package com.example.library.controller;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CorsFallBackController {
-
-    @RequestMapping(value = "/**", method = {
-            RequestMethod.GET,
-            RequestMethod.POST,
-            RequestMethod.PUT,
-            RequestMethod.DELETE,
-            RequestMethod.OPTIONS,
-            RequestMethod.PATCH
-    })
-    public ResponseEntity<?> handleCorsFallback() {
-        return ResponseEntity.ok().build();
-    }
-}
+//package com.example.library.controller;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class CorsFallBackController {
+//
+//    @RequestMapping(value = "/**", method = {
+//            RequestMethod.GET,
+//            RequestMethod.POST,
+//            RequestMethod.PUT,
+//            RequestMethod.DELETE,
+//            RequestMethod.OPTIONS,
+//            RequestMethod.PATCH
+//    })
+//    public ResponseEntity<?> handleCorsFallback() {
+//        return ResponseEntity.ok().build();
+//    }
+//}
