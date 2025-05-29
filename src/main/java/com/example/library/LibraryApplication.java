c17a73f35dc8f7e52508362b894a39219766cb61
az <abdl.alaziz95@gmail.com>
2025-05-29 23:46:33 +0700

    add : swagger config

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @OpenAPIDefinition(
         servers = {
-                @Server(url = "/", description = "Default Server URL")
+                @Server(url = "http://localhost:8080", description = "Local Server"),
+                @Server(url = "https://library-springboot-api-production.up.railway.app", description = "Production")
         }
 )
 @SpringBootApplication
