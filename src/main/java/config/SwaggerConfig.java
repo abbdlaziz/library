da6db1f5e7efc60e908cecad42a609a59010ab9b
az <abdl.alaziz95@gmail.com>
2025-05-29 22:53:12 +0700

    add : swagger config

---
+++ src/main/java/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://library-springboot-api-production.up.railway.app");
+        server.setDescription("Production Server");
+
+        return new OpenAPI().servers(List.of(server));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("default")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
+
